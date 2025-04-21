9a84398bf3b7aba0fa4e185f3673cc289eebf737
Sasha <sashaart.97@gmail.com>
2025-04-21 23:24:29 +0300

    Fixed auto languages select. Fixed related to google translate bugs.

--- scripts/header.js
+++ scripts/header.js
@@ -1,74 +1,91 @@
+const LANGUAGE_STORAGE_KEY = "LANGUAGE_STORAGE_KEY";
 const DEFAULT_LANGUAGE = "en";
 const SUPPORTED_LANGUAGES = {
-  en: ["en", "en-us", "en-gb"],
-  fr: ["fr", "fr-fr", "fr-ca"],
-  de: ["de", "de-de", "de-at"],
-  "zh-CN": ["zh", "zh-cn", "zh-hans"],
-  it: ["it", "it-it"],
+  [DEFAULT_LANGUAGE]: [
+    "en",
+    "en-us",
+    "en-gb",
+    "en-au",
+    "en-ca",
+    "en-in",
+    "en-ie",
+    "en-nz",
+    "en-za",
+    "en-gb-oxendict",
+    "en-gb",
+  ],
+  fr: ["fr", "fr-fr", "fr-ca", "fr-ch"],
+  de: ["de", "de-de", "de-at", "de-li", "de-ch"],
+  "zh-CN": ["zh", "zh-hk", "zh-cn", "zh-tw", "zh-hans"],
+  it: ["it", "it-it", "it-ch"],
 };
-const preferredLanguage = getSystemLanguage() || DEFAULT_LANGUAGE;
 
-const trigger = document.querySelector(".language-select-trigger");
-const options = document.querySelector(".language-select-options");
-const allOptions = document.querySelectorAll(".language-select-option");
-const overlayMenu = document.querySelector(".overlay-menu");
-const hamburgerIcon = document.querySelector(".top-bar .hamburger a");
-
-function getGoogleTranslateTargetLang() {
-  const cookie = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("googtrans="));
-
-  if (!cookie) return null;
-
-  const value = cookie.split("=")[1]; // /en/fr
-  const parts = value.split("/"); // ["", "en", "fr"]
-
-  if (parts.length !== 3) return null;
-
-  const sourceLang = parts[1];
-  const targetLang = parts[2];
-
-  if (targetLang && targetLang !== DEFAULT_LANGUAGE) {
-    return targetLang;
-  }
-
-  return null;
+function getPreviouslySelectedLanguage() {
+  return localStorage.getItem(LANGUAGE_STORAGE_KEY) || null;
 }
 
 function setSelectedOption(option) {
   const img = option.querySelector("img")?.src;
   const text = option.querySelector("span")?.textContent;
+  const trigger = document.querySelector(".language-select-trigger");
   const newLanguageValue = option.dataset?.value;
 
-  trigger.innerHTML = `<img src="${img}" alt="${text}" data-value=${newLanguageValue} /><span>${text}</span>`;
+  trigger.dataset.value = newLanguageValue;
+  trigger.innerHTML = `<img src="${img}" alt="${text}" /><span>${text}</span>`;
 }
 
 function translatePageWithGoogle(option) {
   const googleSelect = document.querySelector(".goog-te-combo");
-  const img = option.querySelector("img")?.src;
-  const text = option.querySelector("span")?.textContent;
+  const trigger = document.querySelector(".language-select-trigger");
+  const selectedValue = trigger.dataset?.value;
+  const options = document.querySelector(".language-select-options");
+
+  const newLanguageImg = option.querySelector("img")?.src;
+  const newLanguageLabel = option.querySelector("span")?.textContent;
   const newLanguageValue = option.dataset?.value;
 
-  if (!googleSelect || !img || !text || !newLanguageValue) {
-    console.log("error, closing select: ", {
-      googleSelect,
-      img,
-      text,
-      newLanguageValue,
-    });
+  if (
+    !googleSelect ||
+    !newLanguageImg ||
+    !newLanguageLabel ||
+    !newLanguageValue ||
+    newLanguageValue === selectedValue
+  ) {
     if (options?.style?.display) {
       options.style.display = "none";
     }
+
     return;
   }
 
-  trigger.innerHTML = `<img src="${img}" alt="${text}" data-value=${newLanguageValue} /><span>${text}</span>`;
+  trigger.dataset.value = newLanguageValue;
+  trigger.innerHTML = `<img src="${newLanguageImg}" alt="${newLanguageLabel}" /><span>${newLanguageLabel}</span>`;
   options.style.display = "none";
 
   googleSelect.value = newLanguageValue;
   googleSelect.dispatchEvent(new Event("change"));
   googleSelect.dispatchEvent(new Event("change"));
+
+  localStorage.setItem(LANGUAGE_STORAGE_KEY, newLanguageValue);
+}
+
+function getSupportedSystemLanguage() {
+  const browserLangs = navigator.languages || [
+    navigator.language || navigator.userLanguage,
+  ];
+  const normalizedLangs = browserLangs.map((lang) => lang.toLowerCase());
+  // console.log("getSupportedSystemLanguage normalizedLangs:", normalizedLangs);
+
+  for (const lang of normalizedLangs) {
+    for (const [key, aliases] of Object.entries(SUPPORTED_LANGUAGES)) {
+      if (aliases.includes(lang)) {
+        // console.log("system language identified:", key, lang);
+        return key;
+      }
+    }
+  }
+
+  return null;
 }
 
 // Initialize Google Translate
@@ -80,38 +97,38 @@ function googleTranslateElementInit() {
 
   new google.translate.TranslateElement(
     {
-      pageLanguage: "en",
+      pageLanguage: DEFAULT_LANGUAGE,
       includedLanguages: "en,fr,de,zh-CN,it",
       autoDisplay: false,
     },
     "google_translate_element"
   );
 
-  const translateLang = getGoogleTranslateTargetLang();
+  const previouslySelectedLanguage = getPreviouslySelectedLanguage();
+  const supportedSystemLanguage = getSupportedSystemLanguage();
 
-  if (translateLang) {
+  if (previouslySelectedLanguage) {
     const option = document.querySelector(
-      `.language-select-options div[data-value="${translateLang}"]`
+      `.language-select-options div[data-value="${previouslySelectedLanguage}"]`
+    );
+    setSelectedOption(option);
+  } else if (
+    supportedSystemLanguage &&
+    supportedSystemLanguage !== DEFAULT_LANGUAGE
+  ) {
+    const option = document.querySelector(
+      `.language-select-options div[data-value="${supportedSystemLanguage}"]`
     );
 
-     setSelectedOption(option);
+    translatePageWithGoogle(option);
   }
 }
 
-function getSystemLanguage() {
-  const browserLangs = navigator.languages || [
-    navigator.language || navigator.userLanguage,
-  ];
-  console.log("browserLangs:", browserLangs);
-  const normalizedLangs = browserLangs.map((lang) => lang.toLowerCase());
-
-  for (const [key, aliases] of Object.entries(SUPPORTED_LANGUAGES)) {
-    if (normalizedLangs.some((l) => aliases.includes(l))) {
-      console.log("system language identified:", key);
-      return key;
-    }
-  }
-}
+const options = document.querySelector(".language-select-options");
+const allOptions = document.querySelectorAll(".language-select-option");
+const overlayMenu = document.querySelector(".overlay-menu");
+const hamburgerIcon = document.querySelector(".top-bar .hamburger a");
+const trigger = document.querySelector(".language-select-trigger");
 
 hamburgerIcon.addEventListener("click", function (e) {
   e.preventDefault();

2102d69a9a97ad70742810d5ddcdccc7dd9a6acb
Sasha <sashaart.97@gmail.com>
2025-04-30 11:37:54 +0300

    Removed garbage code and images. Updated PWA. Improved Lighthouse.

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/reservation.html',
+  '/css/common.css',
+  '/app.js',
+  '/scripts/header.js',
+  '/img/favicon/web-app-manifest-192x192.png',
+  '/img/favicon/web-app-manifest-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
